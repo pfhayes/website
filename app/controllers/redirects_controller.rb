23b5dc505071af5367e9644c8455a9946c7e686b
Patrick Hayes <pfhayes@gmail.com>
2011-11-16 20:01:55 -0500

    Adding mail notifications (do they work though?)

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -1,6 +1,10 @@
 require 'base64'
 require 'digest/md5'
+require 'rest_client'
 
+API_KEY = ENV['MAILGUN_API_KEY']
+API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/mailgun.net"
+    
 class RedirectsController < ApplicationController
   def redirect
     @redirect = get_from_code(params[:code])
@@ -34,17 +38,36 @@ class RedirectsController < ApplicationController
     @redirect = Redirect.new(:url => url, :code => code)
     if @redirect
       @redirect.save()
+      notify_me(@redirect)
       follow @redirect
     else
       fail 'Failed to create redirect' and return
     end
   end
 
+  # Let me know if people start using my URL shortener
+  def notify_me(redirect) 
+    url = get_full_url(redirect)
+    message = "URL shortener was used to shorten #{redirect.url} to " +
+      "<a href=#{url}>#{url}</a>"
+
+    RestClient.post API_URL+"/messages", 
+      :from => "mailer@mailgun.net",
+      :to => "pfhayes@gmail.com",
+      :subject => "URL Shortener used",
+      :text => strip_tags(message),
+      :html => message
+  end
+
   # Use the low order bits of the MD5 hash, and base64/urlencode
   def generate_new_code(url)
     digest = Digest::MD5.hexdigest(url).to_i(16) % 2**32
     base64 = Base64.encode64([digest].pack("N"))
-    return base64.sub(/==\n$/, '').sub('_', '-').sub('/', '+')
+    return base64.gsub(/==\n$/, '').gsub('_', '-').gsub('/', '+')
+  end
+
+  def strip_tags(msg)
+    return msg.gsub(/<\/?[^>]*>/, '')
   end
 
   def get_from_code(code)
@@ -57,5 +80,9 @@ class RedirectsController < ApplicationController
 
   def fail(err_str)
     render :status => :unprocessable_entity, :text => err_str
+  end
+
+  def get_full_url(redirect)
+    return root_url + '/' + redirect.code
   end
 end
