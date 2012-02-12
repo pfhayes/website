550e9178fccae55274e1e6c5efe75e62b45e17f6
Patrick Hayes <pfhayes@gmail.com>
2012-02-12 13:17:01 -0500

    Updating folios controller

--- app/controllers/folios_controller.rb
+++ app/controllers/folios_controller.rb
@@ -1,23 +1,11 @@
 class FoliosController < ApplicationController
-  # GET /folios
-  # GET /folios.xml
+  # GET /portfolio
   def portfolio
     @folios = Folio.find(:all, :order => 'id')
 
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @folios }
-    end
-  end
-
-  # GET /folios/1
-  # GET /folios/1.xml
-  def show
-    @folio = Folio.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @folio }
     end
   end
 end
