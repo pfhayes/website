f10b28a3061cd3c7e54df4281c745dfbeebada83
Patrick Hayes <pfhayes@gmail.com>
2012-11-10 18:30:41 -0500

    More cleaning up

--- app/models/folio.rb
+++
@@ -1,2 +0,0 @@
-class Folio < ActiveRecord::Base
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,3 @@ Redirect.create([{ :code => 'fb', :url => 'https://www.facebook.com/pfhayes' },
                  { :code => 'tw', :url => 'https://www.twitter.com/pfjhayes' },
                  { :code => 'li', :url => 'http://www.linkedin.com/in/pfjhayes' },
                  { :code => 'gh', :url => 'https://www.github.com/pfhayes' }])
-
-Folio.create([
-  { :name => 'GitHub',
-    :description => 'My GitHub repository.',
-    :url => 'https://www.github.com/pfhayes'},
-  { :name => 'WiktionaryMobile',
-    :description => 'The Wikimedia Foundation mobile application for Wiktionary. Worked with a team of students from the University of Waterloo, University of Toronto, and University of British Columbia. Built with PhoneGap.',
-    :url => 'https://www.github.com/wikimedia/WiktionaryMobile'},
-  { :name => 'PubVision',
-    :description => 'PubVision is a hackathon project that three friends and I built. PubVision is a photo booth for businesses that they can install at their venue to encourage fans to engage on Facebook.',
-    :url => 'https://www.facebook.com/pages/PubVision/187061157978938'},
-  { :name => 'Website',
-    :description => "You're looking at it! Built with Rails, hosted on Heroku.",
-    :url => '.' }].reverse)
