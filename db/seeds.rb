b606169357f8975fac064266ad8c41bb214770cf
Patrick Hayes <pfhayes@gmail.com>
2012-11-10 18:45:18 -0500

    Updating seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
-
-Redirect.create([{ :code => 'fb', :url => 'https://www.facebook.com/pfhayes' },
-                 { :code => 'tw', :url => 'https://www.twitter.com/pfjhayes' },
-                 { :code => 'li', :url => 'http://www.linkedin.com/in/pfjhayes' },
-                 { :code => 'gh', :url => 'https://www.github.com/pfhayes' }])
+Redirect.create([{ :code => 'fb', :url => Links::Facebook },
+                 { :code => 'fo', :url => Links::Foursquare },
+                 { :code => 'tw', :url => Links::Twitter },
+                 { :code => 'li', :url => Links::Linkedin },
+                 { :code => 'gh', :url => Links::Github }])
