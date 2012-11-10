a96f570079a42500a95e482d341bb466d14ade33
Patrick Hayes <pfhayes@gmail.com>
2012-11-10 16:57:00 -0500

    style updates

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
-MY_EMAIL='p@patrickhay.es'
+MY_EMAIL = 'p@patrickhay.es'
 API_KEY = ENV['MAILGUN_API_KEY']
 API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/mailgun.net"
 
@@ -6,7 +6,7 @@ module Links
   Twitter = "https://twitter.com/pfjhayes"
   Facebook = "https://facebook.com/pfhayes"
   Linkedin = "https://www.linkedin.com/in/pfjhayes"
-  Foursquare = "https://foursquare.com/pfjayes"
+  Foursquare = "https://foursquare.com/pfjhayes"
   Github = "https://github.com/pfhayes"
 end
 
