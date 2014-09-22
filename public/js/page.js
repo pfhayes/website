632b082db05db4397c3ce2e65a9c1f68e0a0876d
Patrick Hayes <pfhayes@gmail.com>
2014-09-22 16:51:41 -0400

    More logging

--- public/js/page.js
+++ public/js/page.js
@@ -2,6 +2,13 @@ if (mixpanel !== undefined) {
 	var path = document.createElement('a');
 	path.href = document.URL;
 	mixpanel.track("page " + path.pathname);
+
+	$(document).ready(function() {
+		$('a').click(function (event) {
+			var link = $(this);
+			mixpanel.track("link " + link.href);
+		});
+	});
 }
 
 
