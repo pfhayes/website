1666d7c9347f100449f44e027601a69831cbce7f
Patrick Hayes <pfhayes@gmail.com>
2018-01-10 19:33:17 -0800

    More udpates

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Personal::Application.routes.draw do
-    match 'resume' => 'resume#resume', :as => :resume
-    match 'cv' => 'resume#cv', :as => :cv
-    match 'portfolio' => 'folios#portfolio', :as => :portfolio
+    get 'resume' => 'resume#resume', :as => :resume
+    get 'cv' => 'resume#cv', :as => :cv
+    get 'portfolio' => 'folios#portfolio', :as => :portfolio
     post 'shorten' => 'redirects#create', :as => :shorten
     root :to => "home#home"
 
-  # redirecting match
-    match ':code(.:format)' => 'redirects#redirect', :as => :redirect
+    # redirecting match
+    get ':code(.:format)' => 'redirects#redirect', :as => :redirect
 
-  # Nothing left, route to error page
-    match '*wild' => 'application#render_404', :as => :error_404
+    # Nothing left, route to error page
+    get '*wild' => 'application#render_404', :as => :error_404
 end
