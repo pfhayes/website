14aa595e2bf9ec53b6e8d34b24016c564ba2136b
Patrick Hayes <pfhayes@gmail.com>
2018-01-10 19:34:56 -0800

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,11 @@
 Personal::Application.configure do
-  config.cache_classes = true
-  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
-  config.serve_static_files = true
-  config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
+  config.cache_classes = true
+  config.consider_all_requests_local       = false
+  config.eager_load = true
+  config.i18n.fallbacks = true
   config.log_level = :info
+  config.serve_static_files = true
 end
