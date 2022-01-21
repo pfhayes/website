5fde591c2b41257a6020d8266a690fb692c17962
Patrick Hayes <pfhayes@gmail.com>
2022-01-21 12:43:57 -0800

    Mobile layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
     get 'resume' => 'resume#resume', :as => :resume
     get 'cv' => 'resume#cv', :as => :cv
     get 'portfolio' => 'folios#portfolio', :as => :portfolio
+    get 'contact' => 'resume#contact', :as => :contact
     root :to => "home#home"
 
     # redirecting match
