a61a61c900ba9e8820beecc9366c8a8a33d0b634
Patrick Hayes <pfhayes@gmail.com>
2022-12-18 15:09:21 -0800

    Remove unused constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,4 @@
 MY_EMAIL = 'p@phay.es'
-API_KEY = ENV['MAILGUN_API_KEY']
-API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/mailgun.net"
 HOSTS = ['patrickhay.es', 'pfhay.es', 'phay.es']
 CANON_HOST = 'patrickhay.es'
 CANON_URL = 'http://' + CANON_HOST
